1ed48063beca15c9bfeb4ec4581a81b972b36178
Doomtower <jessehudak707@outlook.com>
2022-12-22 07:43:09 -0800

    save point

--- Develop/script.js
+++ Develop/script.js
@@ -1,42 +1,67 @@
+var passwordLength = "8 - 128";
+var lowercaseLetters = "abcdefghijklmnopqrstuvwxyz";
+var numbers = '123456780';
+var symbols = '~!@#$%^&*()_+{}|][';
+var uppercaseLetters = lowercaseLetters.toUpperCase(); // converts lowercase letters to uppercase
+var lowerCaseLettersArray = lowercaseLetters.split(''); // converts letters in a string to an array with those letters (e.g. ['a', 'b', 'c', ...])
+var numbersArray = numbers.split('');
+var symbolsArray = symbols.split('');
+var uppercaseLetters = uppercaseLetters.split('');
+
+
+
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
-// define generatePassword
-function generatePassword(){
-console.log("Button Click")
-// Prompt for password length 
-var passwordLength = prompt("Please select a password length between 8 and 128");
-console.log(passwordLength); {
-if (passwordLength < 8 || passwordLength > 128) {
-  alert("Password length must be between 8 to 128 characters, please enter a different number");
-}};
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword); 
 
-// Prompt for Upper Case 
-var incUpperCase = prompt("Please enter 'YES' or 'NO' if you want to include UPPERCASE characters");
-console.log(incUpperCase);
-
-// Prompt for Numbers 
-var incNumbers = prompt("Please enter 'YES' or 'NO' if you want to include NUMBERS characters");
-console.log(incNumbers);
-
-// Prompt for Special
-var incSpecial = prompt("Please enter 'YES' or 'NO' if you want to include SPECIAL characters");
-console.log(incSpecial);
-
-//Display Answer 
-return "Generated Password will go here!"
-}
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
+  var correctPrompts = getPrompts();
   var passwordText = document.querySelector("#password");
 
-  passwordText.value = password;
-
+  if (correctPrompts){
+    var newPassword = generatePassword();
+    passwordText.value = newPassword;
+  } else {
+    passwordText.value = "";
+  }
 }
 
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword); {
-
+// define generatePassword
+function generatePassword(){
+    var password = "";
+    for (var i = 0; i < passwordLength; i++) {
+    var randChar = Math.floor(Math.random() * choiceArr.length);
+    password = password + choiceArr[randChar];
+  }
+  return password;
 }
+
+function getPrompts () {
+  choiceArr = [];
+  // Prompt for password length 
+  var passwordLength = parseInt(prompt("Please select a password length between 8 and 128"));
+  console.log(passwordLength); {
+      if ((passwordLength) || pass  < 8 || passwordLength > 128) {
+      alert("Password length must be between 8 to 128 characters, please enter a different number");
+       return "Try Again";
+    }
+    if (confirm("Do you want to include UPPERCASE characters")) {
+    choiceArr = (uppercaseLetters + lowerCaseLettersArray);
+    } 
+
+    if (confirm("Do you want to include NUMBERS characters")) {
+    choiceArr = (uppercaseLetters + lowerCaseLettersArray + numbersArray);
+    }
+
+    if (confirm("Do you want to include SPECIAL characters")) {
+    choiceArr = (uppercaseLetters + lowerCaseLettersArray + numbersArray + symbolsArray);
+    } return true;
+  };  
+}
+
+
+ 
