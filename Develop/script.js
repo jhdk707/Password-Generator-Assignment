0be43ecc67fd36b8da16ebbf777968d4579056bb
Doomtower <jessehudak707@outlook.com>
2022-12-21 23:20:06 -0800

    prompts built with console log

--- Develop/script.js
+++ Develop/script.js
@@ -1,6 +1,32 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+// define generatePassword
+function generatePassword(){
+console.log("Button Click")
+// Prompt for password length 
+var passwordLength = prompt("Please select a password length between 8 and 128");
+console.log(passwordLength); {
+if (passwordLength < 8 || passwordLength > 128) {
+  alert("Password length must be between 8 to 128 characters, please enter a different number");
+}};
+
+// Prompt for Upper Case 
+var incUpperCase = prompt("Please enter 'YES' or 'NO' if you want to include UPPERCASE characters");
+console.log(incUpperCase);
+
+// Prompt for Numbers 
+var incNumbers = prompt("Please enter 'YES' or 'NO' if you want to include NUMBERS characters");
+console.log(incNumbers);
+
+// Prompt for Special
+var incSpecial = prompt("Please enter 'YES' or 'NO' if you want to include SPECIAL characters");
+console.log(incSpecial);
+
+//Display Answer 
+return "Generated Password will go here!"
+}
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
@@ -12,8 +38,5 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword); {
-prompt("Please select a password length between 8 and 128");
-prompt("Please enter 'YES' or 'NO' if you want to include UPPERCASE characters");
-prompt("Please enter 'YES' or 'NO' if you want to include NUMBERS characters");
-prompt("Please enter 'YES' or 'NO' if you want to include SPECIAL characters")
+
 }
